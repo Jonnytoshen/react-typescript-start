9baa92dc5beafc530f05e2ae175d05403b12f129
Jonny <1165606849@qq.com>
2019-02-13 14:52:12 +0800

    feat: opt-in support for LESS

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # react-typescript-start
 React development environment based on typescript.
+
+## 添加支持Less
+在开始修改配置之前，先安装编译Less所依赖的loader:
+```
+npm install less-loader less --save-dev
+```
+依赖loader安装完成后，在 /config/webpack.config.js 文件的 style files regexes 区域添加下面两行代码：
+```
+const lessRegex = /\.less$/;
+const lessModuleRegex = /\.module\.less$/;
+```
+接下来我们继续配置编译less的loader。在file loader之前添加以下代码：
+```
+// Opt-in support for LESS (using .less extensions).
+// By default we support LESS Modules with the
+// extensions .module.less
+{
+    test: lessRegex,
+    exclude: sassModuleRegex,
+    use: getStyleLoaders(
+    {
+        importLoaders: 2,
+        sourceMap: isEnvProduction
+        ? shouldUseSourceMap
+        : isEnvDevelopment,
+    },
+    'less-loader'
+    ),
+    // Don't consider CSS imports dead code even if the
+    // containing package claims to have no side effects.
+    // Remove this when webpack adds a warning or an error for this.
+    // See https://github.com/webpack/webpack/issues/6571
+    sideEffects: true,
+},
+// Adds support for CSS Modules, but using LESS
+// using the extension .module.less
+{
+    test: lessModuleRegex,
+    use: getStyleLoaders(
+    {
+        importLoaders: 2,
+        sourceMap: isEnvProduction
+        ? shouldUseSourceMap
+        : isEnvDevelopment,
+        modules: true,
+        getLocalIdent: getCSSModuleLocalIdent,
+    },
+    'less-loader'
+    ),
+},
+```
+现在开发环境已经支持编译Less样式了，我们可以在项目中使用Less来编写样式。
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -39,6 +39,8 @@ const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
+const lessRegex = /\.less$/;
+const lessModuleRegex = /\.module\.less$/;
 
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
@@ -453,6 +455,45 @@ module.exports = function(webpackEnv) {
                 'sass-loader'
               ),
             },
+
+            // Opt-in support for LESS (using .less extensions).
+            // By default we support LESS Modules with the
+            // extensions .module.less
+            {
+              test: lessRegex,
+              exclude: sassModuleRegex,
+              use: getStyleLoaders(
+                {
+                  importLoaders: 2,
+                  sourceMap: isEnvProduction
+                    ? shouldUseSourceMap
+                    : isEnvDevelopment,
+                },
+                'less-loader'
+              ),
+              // Don't consider CSS imports dead code even if the
+              // containing package claims to have no side effects.
+              // Remove this when webpack adds a warning or an error for this.
+              // See https://github.com/webpack/webpack/issues/6571
+              sideEffects: true,
+            },
+            // Adds support for CSS Modules, but using LESS
+            // using the extension .module.less
+            {
+              test: lessModuleRegex,
+              use: getStyleLoaders(
+                {
+                  importLoaders: 2,
+                  sourceMap: isEnvProduction
+                    ? shouldUseSourceMap
+                    : isEnvDevelopment,
+                  modules: true,
+                  getLocalIdent: getCSSModuleLocalIdent,
+                },
+                'less-loader'
+              ),
+            },
+
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
